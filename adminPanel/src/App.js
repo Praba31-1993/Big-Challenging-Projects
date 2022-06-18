c51538096f0ce4328e353b412fcb9de8e3cb65f7
Praba31-1993 <prabamymotowd@gmail.com>
2022-06-18 18:28:18 +0530

    topbar

--- adminPanel/src/App.js
+++ adminPanel/src/App.js
@@ -1,9 +1,11 @@
-
+import Sidebar from "./component/sidebar/Sidebar";
+import Topbar from "./component/topbar/Topbar";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Admin Panel</h1>
-    </div>
+      <Topbar/>
+      <Sidebar/>
+            </div>
   );
 }
 
