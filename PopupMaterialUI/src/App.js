969bbe8cd1f2ba0b5ff699642a13f06e09fb94e9
Praba31-1993 <prabamymotowd@gmail.com>
2022-06-19 20:00:20 +0530

    dropdownpopup

---
+++ PopupMaterialUI/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from './component/Index';
+import BasicModal from './component/Popup';
+
+function App() {
+  return (
+    <div className="App">
+      <Index/>
+      {/* <BasicModal/> */}
+    </div>
+  );
+}
+
+export default App;
