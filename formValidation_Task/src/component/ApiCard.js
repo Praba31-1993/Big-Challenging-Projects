ab51566272f35497d9a308915f50da2e2407589b
Praba31-1993 <prabamymotowd@gmail.com>
2022-07-13 06:05:22 +0530

    ApiCard task success

--- formValidation_Task/src/component/ApiCard.js
+++ formValidation_Task/src/component/ApiCard.js
@@ -16,29 +16,37 @@ export default function ApiCard() {
   useEffect(() => {
     Users();
   }, []);
-
-  const ClickData=(e)=>{
-    console.log("index", index)
+  const ClickData=(i)=>{
+    console.log("index",i)
+    setIndex(i)
+      
   }
   return (
     <>
     <h1>Welcome to Api Datas</h1><hr/>
       <p style={{display:"flex", flexWrap: "wrap"}} >
-        {data.map((list) => {
+        {data.map((list, i) => {
+
           return (
             
-            <Row >
+            <Row key={list.id}>
+
               <Col >
-              <Card style={{ width: "18rem", margin:"5px"}} >
+                
+              <Card style={{ width: "18rem", margin:"5px", border:index===i?"1px solid blue":"1px dotted black"}}>
               <Card.Header>Name: {list.name}</Card.Header>
               <ListGroup variant="flush">
                 <ListGroup.Item>ID: {list.id}</ListGroup.Item>
+                
+                <ListGroup.Item>Index: {i}</ListGroup.Item>
                 <ListGroup.Item>UserName: {list.username}</ListGroup.Item>
                 <ListGroup.Item>Email: {list.email}</ListGroup.Item>
                 <ListGroup.Item>City: {list.address.city}</ListGroup.Item>
                 {/* <ListGroup.Item>{list.email}</ListGroup.Item> */}
                 {/* <ListGroup.Item>{list.email}</ListGroup.Item> */}
+                {/* <button className="btn btn-primary" onClick={ApiData}>Submit</button> */}
               </ListGroup>
+              <button className="btn btn-outline-primary" onClick={e=>ClickData(i)}>Submit</button>
             </Card>
 
               </Col>
