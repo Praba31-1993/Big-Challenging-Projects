e9ae1cf07f347de013b0b808a04bb67065b75950
Praba31-1993 <prabamymotowd@gmail.com>
2022-07-25 00:49:41 +0530

    project setup

--- React Saga Crud Operations/src/App.js
+++ React Saga Crud Operations/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Home from './pages/Home';
+import About from './pages/About';
+import UserInfo from './pages/UserInfo';
+import AddEditUser from './pages/AddEditUser';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/adduser" component={AddEditUser}/>
+        <Route path="/edituser/:id" component={AddEditUser}/>
+        <Route path="/userinfo/:id" component={UserInfo}/>
+        <Route path="/about" component={About}/>
+
+
+      </Switch>
     </div>
+    </BrowserRouter> 
   );
 }
 
